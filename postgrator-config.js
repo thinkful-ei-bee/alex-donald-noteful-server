7143bf031200a8d81621f2685b2ed0c12be38428
Alex Varghese <alex.varghese250@gmail.com>
2019-04-12 17:07:28 -0400

    fixed packages

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,10 +1,11 @@
+'use strict';
 require('dotenv').config();
 
 module.exports = {
-  "migrationDirectory": "migrationDirectory",
-  "driver": "pg",
-  "host": process.env.MIGRATION_DB_HOST,
-  "port": process.env.MIGRATION_DB_PORT,
-  "database": process.env.MIGRATION_DB_NAME,
-  "username": process.env.MIGRATION_DB_USER,
-}
+  'migrationDirectory': 'migrationDirectory',
+  'driver': 'pg',
+  'host': process.env.MIGRATION_DB_HOST,
+  'port': process.env.MIGRATION_DB_PORT,
+  'database': process.env.MIGRATION_DB_NAME,
+  'username': process.env.MIGRATION_DB_USER,
+};
